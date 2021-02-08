55ec158097ed0ea35d8cce5d8020506d33cd5380
mazhar <mazhar_live@hotmail.com>
2021-02-08 02:06:49 +0100

    convert soot from Decimal128 to float

--- sensor-sim/rgen.py
+++ sensor-sim/rgen.py
@@ -3,8 +3,7 @@ from faker import Faker
 import time
 from datetime import datetime
 from datetime import timedelta
-from decimal import Decimal
-from bson.decimal128 import Decimal128
+from random import random
 
 myclient = MongoClient('localhost',27017)
 mydb = myclient.my_database
@@ -12,33 +11,35 @@ mycol = mydb.sensordatas
 
 def create_sensor_data(fake):
     #datentime=datetime(2020,8,1)
-    datentime=datetime.now()
+    # datentime=datetime.now()
     
-    for i in range(15):
-        for j in range(48):   
+    # for i in range(15):
+        # for j in range(48):   
+    while True:        
             # datentime += timedelta(days=1)
-            datentime-=timedelta(hours=0.5)
-            sootData = Decimal128( str(fake.pyfloat(left_digits=3, right_digits=3, positive=False, min_value=0, max_value=999)))
-            sootData= Decimal128(str(sootData))
-            tempData = fake.random_int(-40,85)
-            humidityData = fake.random_int(0,100)
-            pressureData = fake.random_int(300,1250)
-
-            result = mycol.insert_one( 
-                {
-                    
-                    'soot': sootData,
-                    'temperature': tempData,
-                    'humidity': humidityData,
-                    'pressure': pressureData,
-                    'dateCreated': datentime
-                    }
-                )
-
-                # print ('soot:' + str(sootData) + ' temperature:' + str(tempData))
-                # time.sleep(5)
-
-        # datentime += timedelta(days=1)
+            # datentime-=timedelta(hours=0.5)
+        datentime=datetime.now()
+        sootData=random()*1000
+        sootData=float("{0:.2f}".format(sootData))
+        tempData = fake.random_int(-40,85)
+        humidityData = fake.random_int(0,100)
+        pressureData = fake.random_int(300,1250)
+        print(sootData)
+        
+        result = mycol.insert_one( 
+            {
+                
+                'soot': sootData,
+                'temperature': tempData,
+                'humidity': humidityData,
+                'pressure': pressureData,
+                'dateCreated': datentime
+                }
+            )
+        
+        time.sleep(3)
+        
+           
         
 if __name__ == '__main__':
     fake = Faker()
